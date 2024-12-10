edb63f156fd46a9f0821ca18be985628480d9328
steventram <steven.tram@edu.sait.ca>
2024-12-09 17:11:59 -0700

    implement api
    implement api callback and update movie list according to the user's input. Also added clear functionality to the clear the stored movie list

--- app/_components/movie.js
+++ app/_components/movie.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function NewMovie() {
+export default function Movie({ onSearch, onClear }) {
   const [movieName, setMovieName] = useState("");
 
   const handleSubmit = (event) => {
@@ -10,21 +10,25 @@ export default function NewMovie() {
     let movie = { movieName: movieName };
 
     setMovieName("");
+    onSearch(movieName);
 
     alert(`Name: ${movie.movieName}`);
   };
 
   const handleNameChange = (event) => setMovieName(event.target.value);
-  const handleClear = () => setMovieName("");
+  const handleClear = () => {
+    setMovieName("");
+    onClear();
+  };
 
   return (
     <div className="m-1 flex flex-col justify-center items-center">
       <form onSubmit={handleSubmit}>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center m-1">
           <input
             type="text"
             placeholder="Enter Movie Name"
-            className="rounded-md text-center w-full border-2 border-slate-400"
+            className="rounded-md text-center w-full border-2 border-slate-400 m-1"
             onChange={handleNameChange}
             value={movieName}
             required
--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@
 import Movie from "./_components/movie";
 import MovieInfo from "./_components/movieinfo";
 import MovieList from "./_components/movielist";
-import randomMovie from "./_components/inception.json";
+// import randomMovie from "./_components/inception.json";
 import Modal from "./_components/modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Page() {
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [movieList, setMovieList] = useState([]);
 
   const handleSelectMovie = (movie) => {
     setSelectedMovie(movie);
@@ -18,11 +19,38 @@ export default function Page() {
     setSelectedMovie(null);
   };
 
+  const handleSearch = async (query) => {
+    try {
+      const response = await fetch(
+        `https://imdb.iamidiotareyoutoo.com/search?q=${query}`
+      );
+      const data = await response.json();
+
+      if (data.ok) {
+        setMovieList(data.description);
+        console.log(movieList);
+      } else {
+        alert("No Movies Found");
+        setMovieList([]);
+      }
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
+  const handleClear = () => {
+    setMovieList([]);
+    alert("Cleared!");
+  };
+
   return (
     <main className="bg-slate-400 flex flex-row justify-center align-middle">
       <div className="m-2">
-        <Movie />
-        <MovieList data={randomMovie} onSelect={handleSelectMovie} />
+        <Movie onSearch={handleSearch} onClear={handleClear} />
+        <MovieList
+          data={{ description: movieList }}
+          onSelect={handleSelectMovie}
+        />
         {/* <MovieInfo data={randomMovie} /> */}
 
         {selectedMovie && (
